2b3a63b3beaaf7546247b45fb8dc3d899fe21dce
ndasilver <ndasilver@gmail.com>
2020-03-16 19:43:14 +0200

    feat(result pate): - created search result page [Finish #171791541]

---
+++ src/pages/searchResult.tsx
@@ -0,0 +1,27 @@
+import { camera, trash, close, chevronBack, search } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon } from '@ionic/react';
+import React from 'react';
+import ResultContent from '../components/searchResult';
+
+const searchResult: React.FC = () => {
+    return (
+        <IonPage>
+            <IonToolbar>
+                <IonTitle>
+                    {/* <IonIcon icon={chevronBack}></IonIcon> */}
+                    <h5 className="ion-text-center ">Actions</h5>
+                </IonTitle>
+            </IonToolbar>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Blank</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ResultContent />
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default searchResult;
